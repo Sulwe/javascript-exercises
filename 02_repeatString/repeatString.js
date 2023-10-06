2cb06c3890cd049c4159652173dd590654010b26
Sulwe <cypriansulwe.cs@gmail.com>
2023-10-06 22:01:13 +0300

    add cone to multiply string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    let word = '';
+    let i = 0;
+
+    if (number < 0) {
+        return 'ERROR';
+    }
+
+    while (i < number) {
+        word += string;
+        i++;
+    }
+    return word;
 
 };
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
