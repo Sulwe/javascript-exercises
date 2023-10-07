1c4e96667d60890eeda92e8e2c86c2b3ab4a7819
Sulwe <cypriansulwe.cs@gmail.com>
2023-10-08 00:30:48 +0300

    add code to return ERROR for non-number arguments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,11 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (typeof(x) !== Number || typeof(y) !== Number) {
+        return "ERROR";
+    }
+    else if (x < 0 || y < 0){
+        return "ERROR";
+    }
+    
 
 };
 
