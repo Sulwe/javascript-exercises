16f7365e7b5142e5466717bb13b8566327757813
Sulwe <cypriansulwe.cs@gmail.com>
2023-10-07 22:58:18 +0300

    add code to remove given elements

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for (const arg of args){
+            const removedFrom = arr.filter(function(element){
+                return element !== arg;
+
+            });
+                
+            return removedFrom;
+    };
 
 };
 
