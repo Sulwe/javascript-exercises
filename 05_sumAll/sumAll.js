e67e7a7702bc503292a9cea2e7385f042d487a93
Sulwe <cypriansulwe.cs@gmail.com>
2023-10-08 00:39:56 +0300

    correct typo in number and move return value outside for loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,16 @@
 const sumAll = function(x, y) {
-    if (typeof(x) !== Number || typeof(y) !== Number) {
+    if (typeof(x) !== 'number' || typeof(y) !== 'number') {
         return "ERROR";
     }
     else if (x < 0 || y < 0){
         return "ERROR";
+    }
+    else if (x < y){
+        let sum = 0;
+        for (let i = x; i <= y; i++){
+            sum += i;
+        }
+        return sum;
     }
     
 
