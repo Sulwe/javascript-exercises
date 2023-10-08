e5e20b0e932a884e27351e51818b414e41b0aaa6
Sulwe <cypriansulwe.cs@gmail.com>
2023-10-08 16:04:00 +0300

    add code for palindromes and test the code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let splitString = string.split("");
+    let reversedString = splitString.reverse();
+    let newString = reversedString.join("")
+    let punctuationlessString = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    let finalString = punctuationlessString.replace(/\s+/g, '');
+    let punctuationlessNewString = newString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    let finalNewString = punctuationlessNewString.replace(/\s+/g, '');
+
+    return finalNewString.toLowerCase() === finalString.toLowerCase();
 
 };
 
