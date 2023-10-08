ab22d338ccaf06c88f07d36a8e1d360937eab920
Sulwe <cypriansulwe.cs@gmail.com>
2023-10-08 15:18:06 +0300

    add code for calculator and test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,42 @@
-const add = function() {
+const add = function(x, y) {
+  return (parseInt(x) + parseInt(y));
 	
 };
 
-const subtract = function() {
+const subtract = function(x, y) {
+  return (parseInt(x) - parseInt(y));
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+  let total = 0;
+  arr.forEach(element => {
+    total += parseInt(element); 
+  });
+  return total;
 	
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let product = 1;
+  arr.forEach(element => {
+    product *= element;
+  })
+  return product;
 
 };
 
-const power = function() {
+const power = function(x, y) {
+  return(x ** y);
 	
 };
 
-const factorial = function() {
+const factorial = function(x) {
+  if(x === 0){
+    return 1;
+  } else {
+    return (x * factorial(x - 1));
+  }
 	
 };
 
