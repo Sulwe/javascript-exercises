6d8a96486e2d8e2971d28543d5aedcf7cb5c8266
Sulwe <cypriansulwe.cs@gmail.com>
2023-10-08 14:46:23 +0300

    add code to convert temperature and test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  if ((temp - 32)*5/9 % 1 === 0){
+    return((temp - 32)*5/9);
+
+  } else {
+    return(parseFloat(((temp - 32)*5/9).toFixed(1)));
+  }
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  if ((temp*9/5 + 32) % 1 === 0) {
+    return(temp*9/5 + 32);
+  } else {
+    return(parseFloat((temp*9/5 + 32).toFixed(1)));
+  }
 };
 
 // Do not edit below this line
